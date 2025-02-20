be12d051c34e74078efb55ddad8ed22fc227295b
Yevhen Boichuk <geri08311@gmail.com>
2025-02-20 15:24:24 +0200

    fix: add examples

---
+++ main.py
@@ -0,0 +1,20 @@
+import yaml
+import sys
+import os
+import utils
+import generator
+
+def yaml_to_header(yaml_file, header_file, language):
+    generator.gen(yaml_file, header_file, language)
+
+def generate_files(yaml_file):
+    base_name = os.path.splitext(yaml_file)[0]
+    
+    yaml_to_header(yaml_file, f"{sys.argv[2]}", sys.argv[3])
+    print(f"Generated {base_name}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python script.py input.yaml h/hpp/py")
+    else:
+        generate_files(sys.argv[1])
